54bf74ca339d9220882d019f4bdcea74087373de
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-18 18:19:20 +0530

    add: packer.nvim checker

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
+-- Check if packer.nvim is installed
+-- If not then install it
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
 require("general")
