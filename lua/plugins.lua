d8216755caa9e817b93623c49266c367d1900ea0
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-18 18:44:34 +0530

    add: several plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,19 @@
 return require('packer').startup(function()
-    use 'arcticicestudio/nord-vim'
+  use 'arcticicestudio/nord-vim'
+  
+  use 'tpope/vim-surround'
+
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("core.treesitter").setup()
+    end,
+  }
+
+  use {
+    "airblade/vim-rooter",
+    config = function()
+      vim.g.rooter_silent_chdir = 1
+    end,
+  }
 end)
