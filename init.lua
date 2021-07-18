5324a1e28d75a9f3681fabcb515fc229772de12e
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-18 18:32:36 +0530

    add: packer.nvim configuration

--- init.lua
+++ init.lua
@@ -9,4 +9,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   execute 'packadd packer.nvim'
 end
+
 require("general")
+
+require("plugins")
