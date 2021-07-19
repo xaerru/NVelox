1f35717c7fd818aee3ae25f039205608a9215f0f
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-19 05:47:53 +0530

    perf: change how plugins work

--- init.lua
+++ init.lua
@@ -1,16 +1,6 @@
--- Check if packer.nvim is installed
--- If not then install it
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
-end
-
-require("general")
-require("plugins")
+local plugins = require "plugins"
+local loader = require("loader").init()
+loader:load { plugins, loader.user_plugins }
 require("plug_config")
+require("general")
 require("colors")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,26 +1,30 @@
-return require('packer').startup(function()
-  use 'arcticicestudio/nord-vim'
-  
-  use 'tpope/vim-surround'
+return {
+  {"tpope/vim-surround"},
 
-  use {
+  {
     "nvim-treesitter/nvim-treesitter",
     config = function()
       require("core.treesitter").setup()
     end,
-  }
+  },
 
-  use {
+  {
     "airblade/vim-rooter",
     config = function()
       vim.g.rooter_silent_chdir = 1
     end,
-  }
+  },
 
-  use {
-    'hoob3rt/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
+  {
+    "hoob3rt/lualine.nvim",
+    requires = {"kyazdani42/nvim-web-devicons", opt = true}
+  },
 
-  use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
-end)
+  {"akinsho/nvim-bufferline.lua", requires = "kyazdani42/nvim-web-devicons"},
+
+  {
+    "arcticicestudio/nord-vim",
+    event = "ColorSchemePre"
+  },
+  
+}
