a19f9f237a239dff1317b0b88d2582c3d7088b01
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-21 11:01:30 +0530

    fix: lazygit size

--- lua/plug_config/whichkey.lua
+++ lua/plug_config/whichkey.lua
@@ -3,7 +3,7 @@ local wk = require("which-key")
 wk.register({
   t = {
     name = "terminal",
-    g = { "<CMD>FloatermNew --height=0.8 --width=0.8 --position=center lazygit<CR>", "lazygit" },
+    g = { "<CMD>FloatermNew --height=0.9 --width=0.9 --position=center lazygit<CR>", "lazygit" },
     v = { "<CMD>FloatermNew --height=0.5 --width=0.5 --wintype=vsplit<CR>", "Vertical" },
     h = { "<CMD>FloatermNew --height=0.5 --width=0.5 --wintype=split<CR>", "Horizontal" },
     f = { "<CMD>FloatermNew --height=40 --width=0.99<CR>", "Full" },
