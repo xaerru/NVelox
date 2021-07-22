6fdda8a1f3eea6011adc542ca99227635228f20c
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-22 09:47:22 +0530

    add: floating windows instead of splits for terminals

--- lua/plug_config/whichkey.lua
+++ lua/plug_config/whichkey.lua
@@ -4,8 +4,8 @@ wk.register({
   t = {
     name = "terminal",
     g = { "<CMD>FloatermNew --height=0.9 --width=0.9 --position=center lazygit<CR>", "lazygit" },
-    v = { "<CMD>FloatermNew --height=0.5 --width=0.5 --wintype=vsplit<CR>", "Vertical" },
-    h = { "<CMD>FloatermNew --height=0.5 --width=0.5 --wintype=split<CR>", "Horizontal" },
+    v = { "<CMD>FloatermNew --height=40 --width=0.5 --position=right<CR>", "Vertical" },
+    h = { "<CMD>FloatermNew --height=0.5 --width=0.99 --position=top<CR>", "Horizontal" },
     f = { "<CMD>FloatermNew --height=40 --width=0.99<CR>", "Full" },
   },
 }, { prefix = "<leader>" })
