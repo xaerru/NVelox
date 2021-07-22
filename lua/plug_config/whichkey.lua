2a776362502800c02b863543df361336f6ad418d
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-22 10:00:03 +0530

    add: telescope keybinds

--- lua/plug_config/whichkey.lua
+++ lua/plug_config/whichkey.lua
@@ -9,3 +9,14 @@ wk.register({
     f = { "<CMD>FloatermNew --height=40 --width=0.99<CR>", "Full" },
   },
 }, { prefix = "<leader>" })
+
+wk.register({
+  p = {
+    name = "telescope",
+    p = { "<CMD>Telescope<CR>", "Telescope" },
+    f = { "<CMD>Telescope find_files<CR>", "Find Files" },
+    g = { "<CMD>Telescope git_files<CR>", "Git Files" },
+    w = { "<CMD>Telescope grep_string<CR>", "Search string under cursor" },
+    r = { "<CMD>Telescope live_grep<CR>", "Search string" },
+  },
+}, { prefix = "<leader>" })
