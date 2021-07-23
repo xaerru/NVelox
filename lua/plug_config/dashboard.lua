f6c0f32961fa30c33583a4c9565e35854b916e41
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-23 16:03:12 +0530

    refactor: dashboard.nvim

--- lua/plug_config/dashboard.lua
+++ lua/plug_config/dashboard.lua
@@ -4,7 +4,6 @@ g.dashboard_disable_at_vimenter = 0
 g.dashboard_disable_statusline = 1
 g.dashboard_default_executive = "telescope"
 
-
 g.dashboard_custom_header = {
   " ██████╗ ██████╗ ██╗   ██╗██╗  ██╗███████╗",
   "██╔════╝ ██╔══██╗██║   ██║╚██╗██╔╝██╔════╝",
