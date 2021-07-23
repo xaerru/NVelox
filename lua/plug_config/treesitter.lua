dbe4dfa1480fa279484e0b944eba7b680c397364
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-23 10:49:09 +0530

    add: a couple of highlighters

--- lua/plug_config/treesitter.lua
+++ lua/plug_config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"c", "cpp", "rust", "python", "lua", "yaml"},
+  ensure_installed = {"c", "cpp", "rust", "python", "lua", "yaml", "bash", "fish", "tsx", "javascript", "json", "haskell", "typescript", "toml", "html"},
   highlight = {
     enable = true -- false will disable the whole extension
   },
