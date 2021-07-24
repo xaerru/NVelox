63cdf85c09e9a187e2e94cb66e7041f591cec11d
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-24 17:31:47 +0530

    add: windows keybinds

--- lua/plug_config/whichkey.lua
+++ lua/plug_config/whichkey.lua
@@ -97,3 +97,25 @@ wk.register({
     }
   }
 }, {prefix = "<leader>"})
+
+wk.register({
+  w = {
+    name = "windows" ,
+    w = {"<C-W>w"     , "other-window"}          ,
+    q = {"<C-W>c"     , "delete-window"}         ,
+    ["-"] = {"<C-W>s"     , "split-window-below"}    ,
+    ["|"] = {"<C-W>v"     , "split-window-right"}    ,
+    ["2"] = {"<C-W>v"     , "layout-double-columns"} ,
+    h = {"<C-W>h"     , "window-left"}           ,
+    j = {"<C-W>j"     , "window-below"}          ,
+    l = {"<C-W>l"     , "window-right"}          ,
+    k = {"<C-W>k"     , "window-up"}             ,
+    H = {"<C-W>5<"    , "expand-window-left"}    ,
+    J = {":resize +5"  , "expand-window-below"}   ,
+    L = {"<C-W>5>"    , "expand-window-right"}   ,
+    K = {":resize -5"  , "expand-window-up"}      ,
+    ["="] = {"<C-W>="     , "balance-window"}        ,
+    s = {"<C-W>s"     , "split-window-below"}    ,
+    v = {"<C-W>v"     , "split-window-below"}    ,
+  }
+}, {prefix = "<leader>"})
