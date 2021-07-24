20648954ff90dd2ad1c835143997e89feaad6ff1
Gauravsingh Sisodia <xaerru@disroot.org>
2021-07-24 10:25:20 +0530

    add: gitsigns in lualine

--- lua/plug_config/lualine.lua
+++ lua/plug_config/lualine.lua
@@ -2,17 +2,24 @@ local nord = require("lualine.themes.nord")
 
 nord.insert.a.bg = "#A3BE8C"
 
+local function git()
+  local g = vim.b.gitsigns_status_dict
+  if g == nil then
+    return nil
+  else
+    return string.format("+%s ~%s -%s", g.added, g.changed, g.removed)
+      --return tostring(g.added) .. tostring(g.changed) .. tostring(g.removed)
+  end
+end
+
 require"lualine".setup {
   options = {
     icons_enabled = true,
     theme = nord,
-    component_separators = {"", ""},
-    section_separators = {"", ""},
-    disabled_filetypes = {}
   },
   sections = {
     lualine_a = {"mode"},
-    lualine_b = {"branch"},
+    lualine_b = {git},
     lualine_c = {"filename"},
     lualine_x = {"filetype"},
     lualine_y = {},
@@ -24,6 +31,7 @@ require"lualine".setup {
     lualine_c = {"filename"},
     lualine_x = {"location"},
     lualine_y = {},
+
     lualine_z = {}
   },
   tabline = {},
