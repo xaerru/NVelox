3948929332d88f8782ae04a9149f469c899c5135
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-27 14:49:39 +0530

    add: compe auto-pairs

--- lua/plug_config/autopairs.lua
+++ lua/plug_config/autopairs.lua
@@ -6,20 +6,7 @@ require("nvim-autopairs").setup {
     java = false, -- don't check treesitter on java
   },
 }
-
-local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
-
--- skip it, if you use another global object
-_G.MUtils= {}
-
-MUtils.completion_confirm=function()
-  if vim.fn.pumvisible() ~= 0  then
-      return npairs.esc("<cr>")
-  else
-    return npairs.autopairs_cr()
-  end
-end
-
-
-remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
+require("nvim-autopairs.completion.compe").setup({
+  map_cr = true, --  map <CR> on insert mode
+  map_complete = true -- it will auto insert `(` after select function or method item
+})
