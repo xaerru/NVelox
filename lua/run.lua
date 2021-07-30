5b9db3cf2dd712343d9368e2f2d291320b6c4568
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-30 15:41:07 +0530

    add: run.lua

---
+++ lua/run.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+function M.cmd()
+    local expand = vim.fn.expand
+    local file = expand("%:p") -- Absolute path to the file
+    local noext = expand("%:p:r") -- File path without extension
+    local path = expand("%:p:h") -- File path without the name
+    local name = expand("%:t:r") -- File name without the extension
+    local cmd_table = {
+        python = "python " .. file,
+    }
+    return cmd_table[vim.fn.eval("&filetype")]
+end
+
+return M
