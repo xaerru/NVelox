efa851dd85b48962463606844fb2317013c2eabc
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-01 19:18:08 +0530

    fix: visual mode highlighting

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,1 +1,10 @@
 vim.g.colors_name = "nordbuddy"
+require("nordbuddy").colorscheme({
+    -- Italic styled comments
+    italic_comments = true,
+})
+vim.cmd([[
+highlight TSNodeKey cterm=bold ctermfg=198 gui=bold guifg=#8fbcbb
+highlight TSNodeUnmatched ctermfg=242 guifg=#4c566a
+highlight Visual gui=reverse
+]])
