d0bcd0e4160146606457d8ca5d96bc3ba494a349
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-01 14:52:09 +0530

    add: which-key hints for moving lines up and down

--- lua/config/plugins/whichkey.lua
+++ lua/config/plugins/whichkey.lua
@@ -3,6 +3,8 @@ local wk = require("which-key")
 wk.register({
     ["<leader>f"] = { "<CMD>Neoformat | write<CR>", "Format" },
     ["<leader>n"] = { "<CMD>NvimTreeToggle<CR>", "Format" },
+    ["<leader>j"] = "Move Line Down",
+    ["<leader>k"] = "Move Line Up",
     ["<leader><TAB>"] = { "<CMD>:BufferLineCyclePrev<CR>", "Previous Buffer" },
     ["]c"] = "Next Git Hunk",
     ["[c"] = "Previous Git Hunk",
