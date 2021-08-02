e08aca117fa5c28efaf7965e9508c2bf270e8c64
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-02 10:42:44 +0530

    feat: add a bunch of languages to cmd.lua

--- lua/cmd.lua
+++ lua/cmd.lua
@@ -8,8 +8,33 @@ function M.run_cmd()
     local name = expand("%:t:r") -- File name without the extension
     local cmd_table = {
         python = "python3 " .. file,
-        c = "gcc " .. file .. ' && ' .. './a.out' .. ' && /bin/rm ./a.out',
-        cpp = "g++ -std=c++17  " .. file .. ' && ' .. './a.out' .. ' && /bin/rm ./a.out',
+        c = "gcc " .. file .. " && " .. "./a.out" .. " && /bin/rm ./a.out",
+        cpp = "g++ -std=c++17  " .. file .. " && " .. "./a.out" .. " && /bin/rm ./a.out",
+        rust = "rustc " .. file .. " && ./" .. name .. "&& /bin/rm " .. name,
+        haskell = "ghc -dynamic "
+            .. file
+            .. " && ./"
+            .. name
+            .. "&& /bin/rm "
+            .. name
+            .. " "
+            .. name
+            .. ".hi "
+            .. name
+            .. ".o",
+        lua = "lua " .. file,
+        sh = "bash " .. file,
+        perl = "perl " .. file,
+        ruby = "ruby " .. file,
+        javascript = "node " .. file,
+        typescript = "tsc "
+            .. file
+            .. "&& node "
+            .. noext
+            .. ".js"
+            .. "&& /bin/rm "
+            .. noext
+            .. ".js",
     }
     return cmd_table[vim.fn.eval("&filetype")]
 end
