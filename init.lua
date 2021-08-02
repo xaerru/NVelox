740e21e80ea58a5ebe8ecab2dadd7be7a2fe62f2
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-02 14:41:32 +0530

    refactor: remove general module

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
-require("general")
+require("globals")
+require("mappings")
+require("settings")
 local plugins = require("plugins")
 local loader = require("loader").init()
 loader:load({ plugins, loader.user_plugins })
