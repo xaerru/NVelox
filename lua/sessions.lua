d8fa89e4e83de7b4f0677bed2533b69501ec1593
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-04 23:14:13 +0530

    feat(sessions): shorten the path of session

--- lua/sessions.lua
+++ lua/sessions.lua
@@ -1,32 +1,61 @@
+-- TODO refactor all of this
+
 local M = {}
 
 local P = require("persistence")
 
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local function split(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
 function M.sload()
-    require("telescope.builtin").find_files({
+    local files = require("plenary.scandir").scan_dir(vim.fn.stdpath("data") .. "/sessions/")
+    local sessions = {}
+    local paths = {}
+    for _, file in ipairs(files) do
+        local f = vim.fn.fnamemodify(file, ":t")
+        f = split(f, "%%")
+        f = split(f[#f], ".")
+        local t = {}
+        for x = 1, #f - 1 do
+            table.insert(t, f[x])
+        end
+        f = table.concat(t, ".")
+        table.insert(paths, file)
+        table.insert(sessions, f)
+    end
+    local picker = pickers.new({
         prompt_title = "Sessions",
-        cwd = vim.fn.expand(vim.fn.stdpath("data") .. "/sessions/"),
-        attach_mappings = function(prompt_bufnr, map)
-            function load(close)
-                local content = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
-                if close then
-                    require("telescope.actions").close(prompt_bufnr)
-                end
+        finder = finders.new_table({
+            results = sessions,
+        }),
+        sorter = conf.generic_sorter(),
+        attach_mappings = function(prompt_bufnr)
+            actions.select_default:replace(function()
+                local content = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
                 vim.cmd(":bufdo w | bd")
-                P.load(content.cwd .. "/" .. content.value)
-            end
-
-            map("i", "<CR>", function()
-                load(true)
-            end)
-
-            map("n", "<CR>", function()
-                load(true)
+                print(vim.inspect(paths[content.index]))
+                P.load(paths[content.index])
             end)
 
             return true
         end,
     })
+    picker:find()
 end
 
 return M
