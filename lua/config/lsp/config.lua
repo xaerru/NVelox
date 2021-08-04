8aa6b6c680a09c34d614cd5647dbeac0e05f062d
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-04 09:54:19 +0530

    refactor(lsp): make a lsp setup configuration file

---
+++ lua/config/lsp/config.lua
@@ -0,0 +1,34 @@
+CONFIG_PATH = os.getenv("HOME") .. "/.local/share/lunarvim/lvim"
+DATA_PATH = vim.fn.stdpath("data")
+CACHE_PATH = vim.fn.stdpath("cache")
+TERMINAL = vim.fn.expand("$TERMINAL")
+USER = vim.fn.expand("$USER")
+
+return {
+    lua = {
+        cmd = {
+            DATA_PATH .. "/lspinstall/lua/sumneko-lua-language-server",
+            "-E",
+            DATA_PATH .. "/lspinstall/lua/main.lua",
+        },
+        settings = {
+            Lua = {
+                runtime = {
+                    version = "LuaJIT",
+                    path = vim.split(package.path, ";"),
+                },
+                diagnostics = {
+                    globals = { "vim" },
+                },
+                workspace = {
+                    library = {
+                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                    },
+                    maxPreload = 100000,
+                    preloadFileSize = 1000,
+                },
+            },
+        },
+    },
+}
