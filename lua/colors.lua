798c934d86b962d18d8a42ca0ee2f57ac0f45b2c
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-05 12:27:04 +0530

    fix(colors): trailing bg highlight

--- lua/colors.lua
+++ lua/colors.lua
@@ -9,6 +9,7 @@ if colorscheme == "nordbuddy" then
 highlight TSNodeKey cterm=bold ctermfg=198 gui=bold guifg=#8fbcbb
 highlight TSNodeUnmatched ctermfg=242 guifg=#4c566a
 highlight Visual gui=reverse
+highlight Whitespace guibg=#2E3440
 ]])
 else
     vim.cmd("colorscheme " .. colorscheme)
