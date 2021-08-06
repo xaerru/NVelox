bb2da9e6c6e9247946a9a35bdfb45c6470f7d3c2
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-07 00:26:29 +0530

    refactor(nvlx): autocmds.lua

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local cmd = vim.cmd
 
-local autocmds = {
+local default_autocmds = {
     __general = {
         {
             "BufWinEnter",
@@ -29,7 +29,7 @@ local autocmds = {
     },
 }
 
-function M.load()
+function M.load_default(autocmds)
     for name, def in pairs(autocmds) do
         cmd("augroup " .. name)
         cmd("autocmd!")
@@ -37,12 +37,13 @@ function M.load()
             local command = table.concat(vim.tbl_flatten({ "autocmd", autocmd }), " ")
             cmd(command)
         end
-        for _, autocmd in pairs(nvlx.autocmds) do
-            local command = table.concat(vim.tbl_flatten({ "autocmd", autocmd }), " ")
-            cmd(command)
-        end
         cmd("augroup END")
     end
+end
+
+function M.load()
+    M.load_default(default_autocmds)
+    M.load_default(nvlx.autocmds)
 end
 
 return M
