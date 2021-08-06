eaff4d5f64427d33cb2574f1a05356be1a591cc0
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-06 17:47:20 +0530

    feat(nvlx): add custom user highlights

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -24,6 +24,13 @@ function M.load()
         end
         vim.cmd(cmd:sub(1, -2))
     end
+    for group, highlight in pairs(nvlx.general.highlights) do
+        local cmd = string.format("highlight %s ", group)
+        for key, value in pairs(highlight) do
+            cmd = cmd .. string.format("%s=%s ", key, value)
+        end
+        vim.cmd(cmd:sub(1, -2))
+    end
 end
 
 return M
