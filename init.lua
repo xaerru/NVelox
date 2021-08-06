e2c19a1aa6e5aaebed9a28c4e5f9ac1717f39a9d
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-06 16:37:05 +0530

    style: init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("defaults")
 local nvlx_config_file = os.getenv("HOME") .. "/.config/nvlx/config.lua"
-local ok,error = pcall(vim.cmd, "luafile" .. nvlx_config_file)
+local ok, error = pcall(vim.cmd, "luafile" .. nvlx_config_file)
 if not ok then
     print("Please check your " .. nvlx_config_file .. " for correction.")
     print(error)
