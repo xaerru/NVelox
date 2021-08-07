ed3a9952b6f84ec6af5e8787f07e2f3da02edfae
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-08 01:54:49 +0530

    fix(colors): fix all the color problems probably

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,4 +1,11 @@
-require("nordbuddy").colorscheme({
-    italic_comments = true,
-})
-require("highlights").load()
+local present, nordbuddy = pcall(require, "nordbuddy")
+
+if present and nvlx.general.colorscheme == "nordbuddy" then
+    nordbuddy.colorscheme({
+        italic_comments = true
+    })
+    require("highlights").load()
+    return true
+else
+    return false
+end
