447fd71753e34e0d35139a49840e266437902507
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-07 17:20:26 +0530

    feat(nvlx): clean up user config loading

--- init.lua
+++ init.lua
@@ -3,10 +3,12 @@ require("defaults")
 
 -- Load user config
 local default_package_path = package.path
-package.path = package.path .. ";" .. os.getenv("HOME") .. "/.config/?/init.lua"
-package.path = package.path .. ";" .. os.getenv("HOME") .. "/.config/nvlx/?/init.lua"
-package.path = package.path .. ";" .. os.getenv("HOME") .. "/.config/nvlx/?.lua"
+package.path = os.getenv("HOME") .. "/.config/?/init.lua;" .. package.path
 require("nvlx")
+local ok, error = pcall(require, "nvlx")
+if not ok then
+    print(error)
+end
 package.path = default_package_path
 
 -- Load plugins
