a8f1f23117d3b9ba882686a09169a08cbe74cbb4
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-07 17:19:22 +0530

    perf(plugins): remove duplicate loading of highlights

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -24,13 +24,6 @@ function M.load_default(highlights)
         end
         vim.cmd(cmd:sub(1, -2))
     end
-    for group, highlight in pairs(nvlx.general.highlights) do
-        local cmd = string.format("highlight %s ", group)
-        for key, value in pairs(highlight) do
-            cmd = cmd .. string.format("%s=%s ", key, value)
-        end
-        vim.cmd(cmd:sub(1, -2))
-    end
 end
 
 function M.load()
