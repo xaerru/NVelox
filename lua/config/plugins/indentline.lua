d99a13516de65bd4b4f108392e693de1d86df1f6
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-08 01:06:42 +0530

    fix(plugins): indentline config

--- lua/config/plugins/indentline.lua
+++ lua/config/plugins/indentline.lua
@@ -1,7 +1,5 @@
 local g = vim.g
 
 g.indentLine_char = "│"
-g.indentLine_setColors = 0
-g.indentLine_setConceal = 0
-g.indentLine_enabled = 0
-g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
+g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard", "packer" }
+g.indent_blankline_buftype_exclude = { "terminal" }
