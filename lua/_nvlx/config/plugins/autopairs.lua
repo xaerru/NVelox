35c528f902a7160466e0e202479c11707f2df306
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-21 15:10:14 +0530

    refactor(plugins): autopairs.lua

--- lua/_nvlx/config/plugins/autopairs.lua
+++ lua/_nvlx/config/plugins/autopairs.lua
@@ -1,12 +1,7 @@
 require("nvim-autopairs").setup({
     check_ts = true,
-    ts_config = {
-        lua = { "string" }, -- it will not add pair on that treesitter node
-        javascript = { "template_string" },
-        java = false, -- don't check treesitter on java
-    },
 })
 require("nvim-autopairs.completion.compe").setup({
-    map_cr = true, --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` after select function or method item
+    map_cr = true,
+    map_complete = true,
 })
