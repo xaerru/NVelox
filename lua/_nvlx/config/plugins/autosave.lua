d375849c00af05a0d6beb0ce6ee1f0131a323ca8
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-21 15:10:51 +0530

    refactor(plugins): autosave.lua

--- lua/_nvlx/config/plugins/autosave.lua
+++ lua/_nvlx/config/plugins/autosave.lua
@@ -1,14 +1,3 @@
 require("autosave").setup({
-    enabled = true,
     execution_message = "",
-    events = { "InsertLeave", "TextChanged" },
-    conditions = {
-        exists = true,
-        filetype_is_not = {},
-        modifiable = true,
-    },
-    write_all_buffers = false,
-    on_off_commands = true,
-    clean_command_line_interval = 0,
-    debounce_delay = 135,
 })
