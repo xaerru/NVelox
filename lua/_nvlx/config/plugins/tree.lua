e6f16fee9cd4c85dd20b2be67a67c03af39ccbef
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-21 15:13:23 +0530

    refactor(plugins): tree.lua(nvim-tree.lua)

--- lua/_nvlx/config/plugins/tree.lua
+++ lua/_nvlx/config/plugins/tree.lua
@@ -1,16 +1,14 @@
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 local g = vim.g
 
-vim.o.termguicolors = true
-
 g.nvim_tree_side = "left"
 g.nvim_tree_width = 25
 g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "target" }
 g.nvim_tree_gitignore = 1
-g.nvim_tree_auto_ignore_ft = { "dashboard" } -- don't open tree on specific fiypes.
+g.nvim_tree_auto_ignore_ft = { "dashboard" }
 g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 1 -- closes tree when it's the last window
-g.nvim_tree_quit_on_open = 1 -- closes tree when file's opened
+g.nvim_tree_auto_close = 1
+g.nvim_tree_quit_on_open = 1
 g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 1
@@ -19,7 +17,7 @@ g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_root_folder_modifier = ":t"
 g.nvim_tree_tab_open = 0
 g.nvim_tree_allow_resize = 1
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
+g.nvim_tree_add_trailing = 0
 g.nvim_tree_disable_netrw = 1
 g.nvim_tree_hijack_netrw = 1
 g.nvim_tree_update_cwd = 1
@@ -29,7 +27,6 @@ g.nvim_tree_show_icons = {
     git = 1,
     folders = 1,
     files = 1,
-    -- folder_arrows= 1
 }
 g.nvim_tree_icons = {
     default = "",
