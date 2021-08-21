cd576c44b22e527245d5242b3f6e570a0c3ecd36
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-21 15:13:11 +0530

    refactor(plugins): terminal.lua(nvim-toggleterm.lua)

--- lua/_nvlx/config/plugins/terminal.lua
+++ lua/_nvlx/config/plugins/terminal.lua
@@ -1,30 +1,22 @@
 local M = {}
 
 require("toggleterm").setup({
-    -- size can be a number or function which is passed the current terminal
     size = 5,
     open_mapping = [[<F1>]],
-    hide_numbers = true, -- hide the number column in toggleterm buffers
+    hide_numbers = true,
     shade_filetypes = {},
     shade_terminals = true,
-    shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+    shading_factor = "1",
     start_in_insert = true,
-    insert_mappings = true, -- whether or not the open mapping applies in insert mode
+    insert_mappings = true,
     persist_size = true,
     direction = "float",
-    close_on_exit = true, -- close the terminal window when the process exits
-    shell = vim.o.shell, -- change the default shell
-    -- This field is only relevant if direction is set to 'float'
+    close_on_exit = true,
+    shell = vim.o.shell,
     float_opts = {
-        -- The border key is *almost* the same as 'nvim_win_open'
-        -- see :h nvim_win_open for details on borders however
-        -- the 'curved' border is a custom border type
-        -- not natively supported but implemented in this plugin.
         border = "single",
         width = 150,
         height = 35,
-        --row = 0,
-        --col = 0,
         winblend = 25,
         highlights = {
             border = "FloatBorder",
