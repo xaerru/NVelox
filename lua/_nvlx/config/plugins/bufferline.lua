9b8a81699088f7e00245a5978683738df5e5c243
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-21 15:11:10 +0530

    refactor(plugins): bufferline.lua

--- lua/_nvlx/config/plugins/bufferline.lua
+++ lua/_nvlx/config/plugins/bufferline.lua
@@ -1,19 +1,16 @@
 require("bufferline").setup({
     options = {
         numbers = "ordinal",
-        number_style = "superscript", -- buffer_id at index 1, ordinal at index 2
+        number_style = "superscript",
         mappings = false,
-        indicator_icon = "▎",
-        buffer_close_icon = "",
         modified_icon = "●",
-        close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
         max_name_length = 18,
-        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        max_prefix_length = 15,
         tab_size = 18,
         diagnostics = "nvim_lsp",
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        diagnostics_indicator = function(count)
             return "(" .. count .. ")"
         end,
         offsets = { { filetype = "NvimTree", text_align = "left" } },
@@ -21,7 +18,7 @@ require("bufferline").setup({
         show_buffer_close_icons = false,
         show_close_icon = false,
         show_tab_indicators = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+        persist_buffer_sort = true,
         enforce_regular_tabs = false,
         always_show_bufferline = true,
         sort_by = "id",
