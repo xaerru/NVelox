99caec857e937d091022280327dd3b1bcf349869
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-26 00:48:18 +0530

    feat(toggleterm): support user config

--- lua/_nvlx/config/plugins/terminal.lua
+++ lua/_nvlx/config/plugins/terminal.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-require("toggleterm").setup({
+require("toggleterm").setup(require("_nvlx.utils").merge({
     size = 5,
     open_mapping = [[<F1>]],
     hide_numbers = true,
@@ -23,7 +23,7 @@ require("toggleterm").setup({
             background = "Normal",
         },
     },
-})
+}, nvlx.config.plugins.toggleterm))
 
 local Terminal = require("toggleterm.terminal").Terminal
 
