df2d328ec9648722ec62768749e0a17e16da6556
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-26 01:09:25 +0530

    feat(autopairs): support override config

--- lua/_nvlx/config/plugins/autopairs.lua
+++ lua/_nvlx/config/plugins/autopairs.lua
@@ -1,4 +1,4 @@
 require("nvim-autopairs").setup(require("_nvlx.utils").merge({
     check_ts = true,
-}, nvlx.config.plugins.autopairs))
+}, nvlx.config.plugins.autopairs, nvlx.config.plugins_override.autopairs))
 require("nvim-autopairs.completion.compe").setup()
