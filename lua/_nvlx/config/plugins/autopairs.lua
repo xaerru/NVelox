cd3f15549da2b7c11fd25556d5f1ea15c64c1c09
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-26 00:55:15 +0530

    style: autopairs.lua

--- lua/_nvlx/config/plugins/autopairs.lua
+++ lua/_nvlx/config/plugins/autopairs.lua
@@ -1,6 +1,4 @@
-require("nvim-autopairs").setup(
-    require("_nvlx.utils").merge({
-        check_ts = true,
-    }, nvlx.config.plugins.autopairs)
-)
+require("nvim-autopairs").setup(require("_nvlx.utils").merge({
+    check_ts = true,
+}, nvlx.config.plugins.autopairs))
 require("nvim-autopairs.completion.compe").setup()
