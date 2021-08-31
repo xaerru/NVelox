239edc11f4ff717c78d837c94b913bbb16b84906
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-31 10:02:28 +0530

    ci(fix): check

--- utils/check.sh
+++ utils/check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if nvim --headless -c ':qall' 2>&1 | grep -q 'Error'; then echo "Error in opening nvim"; false; fi
-if nvim --headless +PackerCompile -c ':qall' 2>&1 | grep -q 'Error'; then echo "Error in PackerCompile"; false; fi
-if nvim --headless +checkhealth -c ':qall' 2>&1 | grep -q 'Error'; then echo "Error in checkhealth"; false; fi
+if nvim --headless -c ':qall' 2>&1 | grep -q 'Error'; then echo "Error in opening nvim"; exit 101; fi
+if nvim --headless +PackerCompile -c ':qall' 2>&1 | grep -q 'Error'; then echo "Error in PackerCompile"; exit 101; fi
+if nvim --headless +checkhealth -c ':qall' 2>&1 | grep -q 'Error'; then echo "Error in checkhealth"; exit 101; fi
