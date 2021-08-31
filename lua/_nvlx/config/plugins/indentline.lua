0ba97b53b773b7734fdedfc8c7c3eaa37e500a3d
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-31 15:15:09 +0530

    feat(indentline): use lua table config instead of global variables

--- lua/_nvlx/config/plugins/indentline.lua
+++ lua/_nvlx/config/plugins/indentline.lua
@@ -1,4 +1,5 @@
-local g = vim.g
-g.indentLine_char = "│"
-g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard", "packer" }
-g.indent_blankline_buftype_exclude = { "terminal" }
+require("indent_blankline").setup(require("_nvlx.utils").merge({
+    char = "│",
+    filetype_exclude = { "help", "terminal", "dashboard", "packer" },
+    buftype_exclude = { "terminal" },
+}, nvlx.config.plugins.indent_blankline, nvlx.config.plugins_override.indent_blankline))
