ead449b9152bee7bf886a969f0929509144661b0
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-31 14:54:31 +0530

    feat(autopairs): add fast wrap

--- lua/_nvlx/config/plugins/autopairs.lua
+++ lua/_nvlx/config/plugins/autopairs.lua
@@ -1,4 +1,13 @@
 require("nvim-autopairs").setup(require("_nvlx.utils").merge({
     check_ts = true,
+    fast_wrap = {
+        map = "<C-f>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        hightlight = "Search",
+    },
 }, nvlx.config.plugins.autopairs, nvlx.config.plugins_override.autopairs))
 require("nvim-autopairs.completion.compe").setup()
