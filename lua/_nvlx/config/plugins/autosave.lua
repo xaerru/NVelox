6e6509de4a95e7b66026c065a6a7b401dc9094fd
Gauravsingh Sisodia <xaerru@disroot.org>
2021-08-31 15:16:16 +0530

    fix(autosave): add override argument

--- lua/_nvlx/config/plugins/autosave.lua
+++ lua/_nvlx/config/plugins/autosave.lua
@@ -1,3 +1,3 @@
 require("autosave").setup(
-    require("_nvlx.utils").merge({ execution_message = "" }, nvlx.config.plugins.autosave)
+    require("_nvlx.utils").merge({ execution_message = "" }, nvlx.config.plugins.autosave, nvlx.config.plugins_override.autosave)
 )
