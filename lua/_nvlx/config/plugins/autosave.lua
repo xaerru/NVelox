f66a4f75fa0316831caa0ae82b1432a68be2da0e
Gauravsingh Sisodia <xaerru@disroot.org>
2021-09-03 14:41:34 +0530

    style: lua files

--- lua/_nvlx/config/plugins/autosave.lua
+++ lua/_nvlx/config/plugins/autosave.lua
@@ -1,3 +1,7 @@
 require("autosave").setup(
-    require("_nvlx.utils").merge({ execution_message = "" }, nvlx.config.plugins.autosave, nvlx.config.plugins_override.autosave)
+    require("_nvlx.utils").merge(
+        { execution_message = "" },
+        nvlx.config.plugins.autosave,
+        nvlx.config.plugins_override.autosave
+    )
 )
