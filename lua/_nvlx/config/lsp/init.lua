4615dc89cc9ed29d7550cee52867ea31de6adb4b
grvxs <gauravsinghsisodia5@gmail.com>
2021-09-05 16:05:25 +0530

    fix(lsp): trigger autocmd after loading config

--- lua/_nvlx/config/lsp/init.lua
+++ lua/_nvlx/config/lsp/init.lua
@@ -13,9 +13,10 @@ local function setup_servers()
 end
 
 setup_servers()
+vim.cmd("silent! e %")
 
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
 require("lspinstall").post_install_hook = function()
     setup_servers() -- reload installed servers
-    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+    vim.cmd("silent! e %") -- this triggers the FileType autocmd that starts the server
 end
