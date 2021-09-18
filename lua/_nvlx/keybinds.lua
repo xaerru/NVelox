4e7e1744066979f339e9690cd436c28d132c7b44
Gauravsingh Sisodia <xaerru@disroot.org>
2021-09-18 16:11:11 +0530

    feat(plugins): move some of them to one file

--- lua/_nvlx/keybinds.lua
+++ lua/_nvlx/keybinds.lua
@@ -4,6 +4,41 @@ local map = vim.api.nvim_set_keymap
 
 vim.g.mapleader = nvlx.general.leader
 
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col(".") - 1
+    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+        return true
+    else
+        return false
+    end
+end
+
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t("<C-n>")
+    elseif vim.fn.call("vsnip#available", { 1 }) == 1 then
+        return t("<Plug>(vsnip-expand-or-jump)")
+    elseif check_back_space() then
+        return t("<Tab>")
+    else
+        return vim.fn["compe#complete"]()
+    end
+end
+
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t("<C-p>")
+    elseif vim.fn.call("vsnip#jumpable", { -1 }) == 1 then
+        return t("<Plug>(vsnip-jump-prev)")
+    else
+        return t("<S-Tab>")
+    end
+end
+
 local modes = {
     command = "c",
     insert = "i",
@@ -60,6 +95,25 @@ end
 function M.load()
     M.loader(default_keybinds)
     M.loader(nvlx.keybinds.general)
+    map("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
+    map("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
+    map("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
+    map("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
+
+    map("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
+    map("i", "<C-e>", "compe#close('<C-e>')", { noremap = true, silent = true, expr = true })
+    map(
+        "i",
+        "<C-f>",
+        "compe#scroll({ 'delta': +4 })",
+        { noremap = true, silent = true, expr = true }
+    )
+    map(
+        "i",
+        "<C-d>",
+        "compe#scroll({ 'delta': -4 })",
+        { noremap = true, silent = true, expr = true }
+    )
 end
 
 return M
