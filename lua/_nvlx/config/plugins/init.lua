6737b8c0650d851547cee0c9000ca94961e2ce65
Gauravsingh Sisodia <xaerru@disroot.org>
2021-09-18 17:08:33 +0530

    feat(toggleterm): move terminal config file

--- lua/_nvlx/config/plugins/init.lua
+++ lua/_nvlx/config/plugins/init.lua
@@ -293,14 +293,6 @@ M["telescope.nvim"] = {
         selection_caret = "ﰲ ",
         winblend = nvlx.general.winblend,
         borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-        --mappings = {
-        --n = {
-        --["q"] = require("telescope.actions").close,
-        --},
-        --i = {
-        --["<C-e>"] = require("telescope.actions").close,
-        --},
-        --},
     },
 }
 
@@ -313,6 +305,31 @@ M["nvim-treesitter"] = {
     },
     autopairs = {
         enable = true,
+    },
+}
+
+M["toggleterm.nvim"] = {
+    size = 5,
+    open_mapping = [[<F1>]],
+    hide_numbers = true,
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = "1",
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    direction = "float",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+        border = "single",
+        width = 150,
+        height = 35,
+        winblend = nvlx.general.winblend,
+        highlights = {
+            border = "FloatBorder",
+            background = "Normal",
+        },
     },
 }
 
