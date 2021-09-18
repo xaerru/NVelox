e372b59c5c3fcc61f614b72d130cdc2634bfbf0a
Gauravsingh Sisodia <xaerru@disroot.org>
2021-09-18 16:47:55 +0530

    feat(nvim-tree): remove config file

--- lua/_nvlx/plugins.lua
+++ lua/_nvlx/plugins.lua
@@ -146,7 +146,54 @@ return {
         "kyazdani42/nvim-tree.lua",
         cmd = "NvimTreeToggle",
         config = function()
-            require("_nvlx.config.plugins.tree")
+            local g = vim.g
+            g.nvim_tree_width = 25
+            g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "target" }
+            g.nvim_tree_gitignore = 1
+            g.nvim_tree_auto_ignore_ft = { "dashboard" }
+            g.nvim_tree_auto_close = 1
+            g.nvim_tree_quit_on_open = 1
+            g.nvim_tree_follow = 1
+            g.nvim_tree_indent_markers = 1
+            g.nvim_tree_hide_dotfiles = 1
+            g.nvim_tree_git_hl = 1
+            g.nvim_tree_highlight_opened_files = 0
+            g.nvim_tree_root_folder_modifier = ":t"
+            g.nvim_tree_tab_open = 0
+            g.nvim_tree_allow_resize = 1
+            g.nvim_tree_add_trailing = 0
+            g.nvim_tree_disable_netrw = 1
+            g.nvim_tree_hijack_netrw = 1
+            g.nvim_tree_update_cwd = 1
+            g.nvim_tree_lsp_diagnostics = 1
+            g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
+            g.nvim_tree_icons = {
+                default = "",
+                symlink = "",
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+                folder = {
+                    default = "",
+                    open = "",
+                    empty = "", -- 
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                lsp = {
+                    hint = "",
+                    info = "",
+                    warning = "",
+                    error = "",
+                },
+            }
         end,
     },
 
