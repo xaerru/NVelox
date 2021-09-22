b8512a6c0508a089cbb0f05f3361f01737e6cc12
Gauravsingh Sisodia <xaerru@disroot.org>
2021-09-22 18:14:51 +0530

    feat(nvlx): add autocmds to new config method

--- lua/_nvlx/config/core.lua
+++ lua/_nvlx/config/core.lua
@@ -242,4 +242,44 @@ M.maps = {
     },
 }
 
+M.autocmds = {
+    __general = {
+        {
+            "BufWinEnter",
+            "*",
+            ":set formatoptions-=cro",
+        },
+        {
+            "TextYankPost",
+            "*",
+            "silent! lua require'vim.highlight'.on_yank({timeout = 200})",
+        },
+        {
+            "VimResized",
+            "*",
+            "wincmd =",
+        },
+    },
+    __terminal = {
+        {
+            "TermEnter",
+            "*",
+            "silent! wa!",
+        },
+    },
+    __dashboard = {
+        {
+            "FileType",
+            "dashboard",
+            [[setlocal fillchars=fold:\ ,vert:\│,eob:\ ,msgsep:‾]],
+        },
+
+        {
+            "FileType",
+            "dashboard",
+            [[nnoremap <silent> <buffer> q :q<CR>]],
+        },
+    },
+}
+
 return M
