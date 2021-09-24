8ab3cdd9485d9644e74ecb809e8983d42ae974c1
Gauravsingh Sisodia <xaerru@disroot.org>
2021-09-24 11:03:52 +0530

    fix(user): typo

--- lua/_nvlx/user.lua
+++ lua/_nvlx/user.lua
@@ -8,7 +8,7 @@ function M:init()
 
     -- Load user config
     local default_package_path = package.path
-    package.path = string.format("%s/.config/?/init.lua", HOME) .. package.path
+    package.path = string.format("%s/.config/?/init.lua;", HOME) .. package.path
     local ok, nvlx = pcall(require, "nvlx")
     if not ok then
         nvlx = {}
