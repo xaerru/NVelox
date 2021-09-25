3ed5237745971f5fc77e2888a8329d731ef12b30
xaerru <xaerru@gmail.com>
2021-09-25 17:46:19 +0530

    feat(options): type checking

--- src/nvelox/options.c
+++ src/nvelox/options.c
@@ -9,9 +9,13 @@ set_options (lua_State *L, int t)
 {
     lua_pushnil (L);
     while (lua_next (L, t) != 0) {
-        // uses 'key' (at index -2) and 'value' (at index -1)
-        set_option_value(lua_tostring(L, -2), lua_toboolean(L, -1), NULL, 0);
-        // removes 'value'; keeps 'key' for next iteration
+        if (lua_type (L, -1) == LUA_TNUMBER) {
+            set_option_value (lua_tostring (L, -2), lua_tonumber (L, -1), NULL, 0);
+        } else if (lua_type (L, -1) == LUA_TSTRING) {
+            set_option_value (lua_tostring (L, -2), 0, lua_tostring (L, -1), 0);
+        } else if(lua_type (L, -1) == LUA_TBOOLEAN) {
+            set_option_value (lua_tostring (L, -2), lua_toboolean (L, -1), NULL, 0);
+        }
         lua_pop (L, 1);
     }
 }
@@ -19,9 +23,9 @@ set_options (lua_State *L, int t)
 void
 options_load (lua_State *L)
 {
-    lua_getglobal(L, "require");
-    lua_pushstring(L, "_nvlx.config.core");
-    lua_call(L, 1, 1);
+    lua_getglobal (L, "require");
+    lua_pushstring (L, "_nvlx.config.core");
+    lua_call (L, 1, 1);
     lua_getfield (L, 2, "options");
-    set_options(L, 3);
+    set_options (L, 3);
 }
