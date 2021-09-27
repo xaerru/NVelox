58f5da55b2da2115a7d62e736c343506cf3987d3
xaerru <xaerru@gmail.com>
2021-09-27 22:17:05 +0530

    chore: add stack comments

--- src/nvelox/core/options.c
+++ src/nvelox/core/options.c
@@ -1,13 +1,16 @@
 #include "luajit/lua.h"
 #include "nvelox/core/options.h"
-#include "nvim.h"
 #include "nvelox/utils/printstack.h"
+#include "nvim.h"
 
 void
 set_options (lua_State *L, int t)
 {
+    // stack = [nvlx, nvlx.options]
     lua_pushnil (L);
+    // stack = [nvlx, nvlx.options, nil]
     while (lua_next (L, t) != 0) {
+        // stack = [nvlx, nvlx.options, key, value]
         const char *key = lua_tolstring (L, -2, 0);
         switch (lua_type (L, -1)) {
             case LUA_TNUMBER:
@@ -23,13 +26,18 @@ set_options (lua_State *L, int t)
                 break;
         }
         lua_pop (L, 1);
+        // stack = [nvlx, nvlx.options, key]
     }
+    print_stack (L);
 }
 
 void
 options_load (lua_State *L)
 {
+    // stack = [nvlx]
     lua_getfield (L, 1, "options");
+    // stack = [nvlx, nvlx.options]
     set_options (L, 2);
-    lua_pop(L, 1);
+    lua_pop (L, 1);
+    // stack = [nvlx]
 }
