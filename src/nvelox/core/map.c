c0857285a8137510cfd07dc4e0e84481a9f1740e
xaerru <xaerru@gmail.com>
2021-09-29 15:24:26 +0530

    feat(maps): add WIP map wrapper

---
+++ src/nvelox/core/map.c
@@ -0,0 +1,42 @@
+#include "luajit/lua.h"
+#include "nvelox/core/options.h"
+#include "nvelox/utils/printstack.h"
+#include "nvim.h"
+#include <stdio.h>
+#include <string.h>
+
+void
+set_maps (lua_State *L, int t)
+{
+    // stack = [nvlx, nvlx.options]
+    lua_pushnil (L);
+    // stack = [nvlx, nvlx.options, nil]
+    while (lua_next (L, t) != 0) {
+        // stack = [nvlx, nvlx.options, key, value]
+        const char *mode = lua_tolstring (L, -2, 0);
+        if (strcmp (mode, "insert") == 0) {
+            lua_pushnil(L);
+            while (lua_next (L, 5) != 0) {
+                /*print_stack(L);*/
+                printf("%s = %s\n", lua_tolstring(L, -2, 0), lua_tolstring(L, -1, 0));
+                break;
+            }
+            break;
+        }
+        lua_pop (L, 1);
+        // stack = [nvlx, nvlx.options, key]
+    }
+}
+
+void
+maps_load (lua_State *L)
+{
+    // stack = [nvlx]
+    lua_getfield (L, 1, "maps");
+    // stack = [nvlx, nvlx.maps]
+    lua_getfield (L, 2, "general");
+    // stack = [nvlx, nvlx.maps, nvlx.maps.general]
+    set_maps (L, 3);
+    lua_pop (L, 2);
+    // stack = [nvlx]
+}
