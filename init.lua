2462b2bec816485781e59f25297c123c0fd043dd
xaerru <xaerru@gmail.com>
2021-09-30 11:48:54 +0530

    remove: call to lua map wrapper

--- init.lua
+++ init.lua
@@ -10,5 +10,5 @@ require("_nvlx.loader").load({ require("_nvlx.plugins"), nvlx.plugins }, nvlx.di
 -- Load default options, kebinds, autocmds
 require("_nvlx.build.init").setup(nvlx)
 -- require("_nvlx.options").load(nvlx.options)
-require("_nvlx.maps").load(nvlx.maps.general)
+--require("_nvlx.maps").load(nvlx.maps.general)
 require("_nvlx.autocmds").load(nvlx.autocmds)
