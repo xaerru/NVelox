edd4d082da040a7b813ff053e1f83c28a787db15
xaerru <xaerru@gmail.com>
2021-10-07 00:04:58 +0530

    feat(nvim): add highlight wrapper

--- src/include/nvim.h
+++ src/include/nvim.h
@@ -90,4 +90,15 @@ extern char* set_option_value(const char* const name,
 ///
 extern int do_map(int maptype, char_u* arg, int mode, bool is_abbrev);
 
+/// Handle ":highlight" command
+///
+/// When using ":highlight clear" this is called recursively for each group with
+/// forceit and init being both true.
+///
+/// @param[in]  line  Command arguments.
+/// @param[in]  forceit  True when bang is given, allows to link group even if
+///                      it has its own settings.
+/// @param[in]  init  True when initializing.
+extern void do_highlight(const char *line, const bool forceit, const bool init);
+
 #endif  // NVIM_H
