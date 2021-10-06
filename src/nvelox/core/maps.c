3186f14f4bfe5f2c403bd1cccdf1c68648a3b77a
xaerru <xaerru@gmail.com>
2021-10-06 15:34:06 +0530

    feat(maps): make my own get_mode_flag

--- src/nvelox/core/maps.c
+++ src/nvelox/core/maps.c
@@ -5,19 +5,46 @@
 #include <stdio.h>
 #include <string.h>
 
+int
+get_mode_flag (const char modec)
+{
+    switch (modec) {
+        case 'i':
+            return INSERT;
+        case 'c':
+            return CMDLINE;
+        case 'l':
+            return LANGMAP;
+        case 'n':
+            return NORMAL;
+        case 'o':
+            return OP_PENDING;
+        case 's':
+            return SELECTMODE;
+        case 't':
+            return TERM_FOCUS;
+        case 'v':
+            return VISUAL + SELECTMODE;
+        case 'x':
+            return VISUAL;
+        default:
+            return VISUAL + SELECTMODE + NORMAL + OP_PENDING;  // Default for :map
+    }
+}
+
 void
 set_mode_maps (lua_State *L)
 {
-    /*const char* mode = lua_tostring(L, 3);*/
-    /*const char modec = mode[0];*/
+    const char *mode = lua_tostring (L, 3);
+    const char modec = mode[0];
+    int mode_flag = get_mode_flag (modec);
     lua_pushnil (L);
-    /*const char *mode = lua_tostring (L, 3);*/
     while (lua_next (L, 4) != 0) {
         const char *key = lua_tostring (L, -2);
         const char *value = lua_tostring (L, -1);
         char_u keymap[strlen (key) + strlen (value) + 2];
-        snprintf ((char *)keymap, sizeof (keymap), "%s %s", key, value);
-        do_map (2, (char_u *)keymap, NORMAL, false);
+        snprintf ((char *)keymap, sizeof(keymap), "%s %s", key, value);
+        do_map (2, (char_u *)keymap, mode_flag, false);
         lua_pop (L, 1);
     }
 }
