009385a5db12eaa3623e69acd63f8affd9fbf618
Gauravsingh Sisodia <xaerru@disroot.org>
2021-10-23 00:30:23 +0530

    refactor(commands): rename functions

--- src/nvelox/core/commands.c
+++ src/nvelox/core/commands.c
@@ -7,13 +7,14 @@
 
 // TODO: Add more options
 void
-set_commands (lua_State *L)
 nv_def_command (const char *name, const char *cmd, int forceit)
 {
     uc_add_command ((char_u *)name, strlen (name), (char_u *)cmd, EX_BANG | EX_EXTRA, -1, 0, 0,
                     (char_u *)"", ADDR_LINES, forceit);
 }
 
+void
+l_set_commands (lua_State *L)
 {
     lua_pushnil (L);
     while (lua_next (L, 2)) {
@@ -23,12 +24,12 @@ nv_def_command (const char *name, const char *cmd, int forceit)
 }
 
 void
-commands_load (lua_State *L)
+l_commands_load (lua_State *L)
 {
     // stack = [nvlx]
     lua_getfield (L, 1, "commands");
     // stack = [nvlx, nvlx.commands]
-    set_commands (L);
+    l_set_commands (L);
     lua_pop (L, 1);
     // stack = [nvlx]
 }
