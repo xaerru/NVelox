3b3f5d6c66b06dcd0c84bd631b421bb8f2191294
xaerru <xaerru@gmail.com>
2021-10-23 00:30:04 +0530

    feat(commands): add lib function

--- src/nvelox/core/commands.c
+++ src/nvelox/core/commands.c
@@ -5,14 +5,19 @@
 #include <stdio.h>
 #include <string.h>
 
+// TODO: Add more options
 void
 set_commands (lua_State *L)
+nv_def_command (const char *name, const char *cmd, int forceit)
+{
+    uc_add_command ((char_u *)name, strlen (name), (char_u *)cmd, EX_BANG | EX_EXTRA, -1, 0, 0,
+                    (char_u *)"", ADDR_LINES, forceit);
+}
+
 {
     lua_pushnil (L);
     while (lua_next (L, 2)) {
-        char_u* name = (char_u*)lua_tostring(L, -2);
-        uc_add_command (name, strlen((char*)name), (char_u *)lua_tostring(L, -1),
-                        EX_BANG | EX_EXTRA, -1, 0, 0, (char_u *)"", ADDR_LINES, false);
+        nv_def_command (lua_tostring (L, -2), lua_tostring (L, -1), false);
         lua_pop (L, 1);
     }
 }
