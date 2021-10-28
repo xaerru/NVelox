0efe7140d4fc5eeee4299ef1df6af86e47453260
xaerru <xaerru@gmail.com>
2021-10-28 08:28:59 +0530

    fix(nvim-tree): deprecations

--- lua/_nvlx/plugins.lua
+++ lua/_nvlx/plugins.lua
@@ -148,46 +148,20 @@ return {
         config = function()
             local g = vim.g
             g.nvim_tree_gitignore = 1
-            g.nvim_tree_auto_ignore_ft = { "dashboard" }
-            g.nvim_tree_quit_on_open = 1
-            g.nvim_tree_indent_markers = 1
             g.nvim_tree_hide_dotfiles = 1
-            g.nvim_tree_git_hl = 1
-            g.nvim_tree_highlight_opened_files = 0
             g.nvim_tree_root_folder_modifier = ":t"
-            g.nvim_tree_add_trailing = 0
-            g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
-            g.nvim_tree_icons = {
-                default = "",
-                symlink = "",
-                git = {
-                    unstaged = "✗",
-                    staged = "✓",
-                    unmerged = "",
-                    renamed = "➜",
-                    untracked = "★",
-                    deleted = "",
-                    ignored = "◌",
-                },
-                folder = {
-                    default = "",
-                    open = "",
-                    empty = "", -- 
-                    empty_open = "",
-                    symlink = "",
-                    symlink_open = "",
-                },
-                lsp = {
-                    hint = "",
-                    info = "",
-                    warning = "",
-                    error = "",
-                },
-            }
             require("nvim-tree").setup({
                 hijack_cursor = true,
                 update_cwd = true,
-                lsp_diagnostics = true,
+                diagnostics = {
+                    enable = true,
+                    icons = {
+                        hint = "",
+                        info = "",
+                        warning = "",
+                        error = "",
+                    },
+                },
                 update_focused_file = {
                     enable = false,
                     update_cwd = false,
