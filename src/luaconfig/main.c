6ce7f2137127150e91b3ee884acafbcc9cbc2188
xaerru <xaerru@gmail.com>
2021-10-29 15:09:35 +0530

    feat(nvelox): add the library with some boilerplate

--- src/luaconfig/main.c
+++ src/luaconfig/main.c
@@ -7,6 +7,9 @@
 #include "luaconfig/core/options.h"
 #include "luaconfig/utils/printstack.h"
 #include "nvim.h"
+#include <stdio.h>
+
+int add (int a, int b);
 
 int
 lua_setup (lua_State *L)
@@ -23,6 +26,7 @@ lua_setup (lua_State *L)
 LUA_API int
 luaopen_nvelox_init (lua_State *L)
 {
+    printf ("%d\n", add (5, 10));
     luaL_Reg fns[] = { { "setup", lua_setup }, { NULL, NULL } };
     luaL_newlib (L, fns);
     return 1;
