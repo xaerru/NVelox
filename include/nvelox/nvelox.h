3a9c9001013c58ff93c55470c3c83ddb663dd48a
Gauravsingh Sisodia <xaerru@disroot.org>
2021-11-08 22:17:20 +0530

    feat(maps): add maps to library

--- include/nvelox/nvelox.h
+++ include/nvelox/nvelox.h
@@ -20,15 +20,8 @@ void nv_do_autocmd(const char* event,
 
 void nv_do_augroup(const char* name, int forceit);
 
-// Flags for modes
-#define NORMAL 0x01
-#define SELECTMODE 0x1000
-#define TERM_FOCUS 0x2000
-#define VISUAL 0x02
-#define OP_PENDING 0x04
-#define CMDLINE 0x08
-#define INSERT 0x10
-#define LANGMAP 0x20
+void
+nv_set_keymap (const char *mode, const char *key, const char *value, bool noremap, bool forceit);
 
 // Shorthand for unsigned variables. Many systems, but not all, have u_char
 // already defined, so we use char_u to avoid trouble.
@@ -49,54 +42,6 @@ extern void nvim_err_write(String str);
 #define OPT_GLOBAL 2
 #define OPT_LOCAL 4
 
-/// Set or remove a mapping or an abbreviation in the current buffer, OR
-/// display (matching) mappings/abbreviations.
-///
-/// ```vim
-/// map[!]                          " show all key mappings
-/// map[!] {lhs}                    " show key mapping for {lhs}
-/// map[!] {lhs} {rhs}              " set key mapping for {lhs} to {rhs}
-/// noremap[!] {lhs} {rhs}          " same, but no remapping for {rhs}
-/// unmap[!] {lhs}                  " remove key mapping for {lhs}
-/// abbr                            " show all abbreviations
-/// abbr {lhs}                      " show abbreviations for {lhs}
-/// abbr {lhs} {rhs}                " set abbreviation for {lhs} to {rhs}
-/// noreabbr {lhs} {rhs}            " same, but no remapping for {rhs}
-/// unabbr {lhs}                    " remove abbreviation for {lhs}
-///
-/// for :map   mode is NORMAL + VISUAL + SELECTMODE + OP_PENDING
-/// for :map!  mode is INSERT + CMDLINE
-/// for :cmap  mode is CMDLINE
-/// for :imap  mode is INSERT
-/// for :lmap  mode is LANGMAP
-/// for :nmap  mode is NORMAL
-/// for :vmap  mode is VISUAL + SELECTMODE
-/// for :xmap  mode is VISUAL
-/// for :smap  mode is SELECTMODE
-/// for :omap  mode is OP_PENDING
-/// for :tmap  mode is TERM_FOCUS
-///
-/// for :abbr  mode is INSERT + CMDLINE
-/// for :iabbr mode is INSERT
-/// for :cabbr mode is CMDLINE
-/// ```
-///
-/// @param maptype  0 for |:map|, 1 for |:unmap|, 2 for |noremap|.
-/// @param arg      C-string containing the arguments of the map/abbrev
-///                 command, i.e. everything except the initial `:[X][nore]map`.
-///                 - Cannot be a read-only string; it will be modified.
-/// @param mode   Bitflags representing the mode in which to set the mapping.
-///               See @ref get_map_mode.
-/// @param is_abbrev  True if setting an abbreviation, false otherwise.
-///
-/// @return 0 on success. On failure, will return one of the following:
-///         - 1 for invalid arguments
-///         - 2 for no match
-///         - 4 for out of mem (deprecated, WON'T HAPPEN)
-///         - 5 for entry not unique
-///
-extern int do_map(int maptype, char_u* arg, int mode, bool is_abbrev);
-
 /// Handle ":highlight" command
 ///
 /// When using ":highlight clear" this is called recursively for each group with
@@ -107,9 +52,6 @@ extern int do_map(int maptype, char_u* arg, int mode, bool is_abbrev);
 ///                      it has its own settings.
 /// @param[in]  init  True when initializing.
 extern void do_highlight(const char* line, const bool forceit, const bool init);
-
-/// ":augroup {name}".
-extern void do_augroup(char_u* arg, int del_group);
 
 // values for cmd_addr_type
 typedef enum {
