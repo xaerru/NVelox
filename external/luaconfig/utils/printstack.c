91a3e50c33df115728bf5eebda109eac36b69046
Gauravsingh Sisodia <xaerru@disroot.org>
2021-11-09 09:26:19 +0530

    refactor(luaconfig): remove luajit headers from project and use system headers

--- external/luaconfig/utils/printstack.c
+++ external/luaconfig/utils/printstack.c
@@ -1,6 +1,6 @@
 // From https://gist.github.com/tylerneylon/5b9ab5ffcc616cd2a337
 
-#include "lauxlib.h"
+#include <luajit-2.1/lauxlib.h>
 #include "printstack.h"
 #include <assert.h>
 #include <ctype.h>
