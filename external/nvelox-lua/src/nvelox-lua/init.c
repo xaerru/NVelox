a266d03e0ddc86e86f1344dce95269a3f55bbefe
Gauravsingh Sisodia <xaerru@disroot.org>
2021-11-29 15:07:03 +0530

    feat(nvelox-lua): add simple option function

--- external/nvelox-lua/src/nvelox-lua/init.c
+++ external/nvelox-lua/src/nvelox-lua/init.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <luajit-2.1/lauxlib.h>
+#include <luajit-2.1/luaconf.h>
+#include <luajit-2.1/lua.h>
+#include <nvelox/nvelox.h>
 
-int main() {
+int
+set_option (lua_State *L)
+{
+    const char* key = luaL_checkstring(L, 1);
+    const char* value = luaL_checkstring(L, 1);
+    nv_set_option(key, value, 0, OPT_BOTH);
     return 0;
+}
+
+LUA_API int luaopen_nvelox(lua_State* L) {
+    luaL_Reg fns[] = { { "set", set_option}, { NULL, NULL } };
+    luaL_newlib (L, fns);
+    return 1;
 }
