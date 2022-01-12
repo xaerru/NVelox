549dc6925a9b7bf13f14d50cb81d5135542e391a
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-13 00:29:03 +0530

    feat(nix): add nvelox-neovim as a build input

--- default.nix
+++ default.nix
@@ -1,9 +1,20 @@
 with (import <nixpkgs> {});
 
+let
+nvelox-neovim = pkgs.neovim.overrideAttrs (oa: {
+  name = "nvelox-neovim";
+  src = fetchFromSourcehut {
+    owner = "~xaerru";
+    repo = "nvelox-neovim";
+    rev = "08bbea4ae1ee8266e637546dfe29944fbf78e3cb";
+    sha256 = "hqcwZjHRMFmXiTAKBs9Yqfml10S8F5we4S56ovrKkZo=";
+  };
+});
+in
 stdenv.mkDerivation {
   name = "nvelox";
   src = ./.;
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake nvelox-neovim ];
   makeTarget = "nvelox";
   enableParallelBuilding = true;
 }
