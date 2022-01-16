b1ed48462e7f2cd5bc31236967bf0e3b8614555c
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-16 23:13:53 +0530

    feat: update nvelox derivation

--- default.nix
+++ default.nix
@@ -1,20 +1,9 @@
 with (import <nixpkgs> {});
 
-let
-nvelox-neovim = pkgs.neovim.overrideAttrs (oa: {
-  name = "nvelox-neovim";
-  src = fetchFromSourcehut {
-    owner = "~xaerru";
-    repo = "nvelox-neovim";
-    rev = "08bbea4ae1ee8266e637546dfe29944fbf78e3cb";
-    sha256 = "hqcwZjHRMFmXiTAKBs9Yqfml10S8F5we4S56ovrKkZo=";
-  };
-});
-in
 stdenv.mkDerivation {
   name = "nvelox";
   src = ./.;
-  nativeBuildInputs = [ cmake nvelox-neovim ];
+  buildInputs = [ cmake ];
   makeTarget = "nvelox";
   enableParallelBuilding = true;
 }
