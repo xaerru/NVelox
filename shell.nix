4fe73d1573f35755c257881e5c577f0830b76fd3
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-16 23:14:19 +0530

    refactor: shell.nix

--- shell.nix
+++ shell.nix
@@ -1,16 +1,5 @@
 with (import <nixpkgs> {});
 
-let
-nvelox-neovim = pkgs.neovim.overrideAttrs (oa: {
-  name = "nvelox-neovim";
-  src = fetchFromSourcehut {
-    owner = "~xaerru";
-    repo = "nvelox-neovim";
-    rev = "08bbea4ae1ee8266e637546dfe29944fbf78e3cb";
-    sha256 = "hqcwZjHRMFmXiTAKBs9Yqfml10S8F5we4S56ovrKkZo=";
-  };
-});
-in
 mkShell {
-  nativeBuildInputs = [nvelox-neovim];
+  nativeBuildInputs = [(import ./default.nix) (import ./nvelox-neovim.nix)];
 }
