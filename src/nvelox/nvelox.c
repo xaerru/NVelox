fbe90c0b0092e6645776979de62d2f325d9dd4c2
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-20 19:14:26 +0530

    feat(nvelox): add a function for loading C plugins

--- src/nvelox/nvelox.c
+++ src/nvelox/nvelox.c
@@ -1,5 +1,21 @@
+#include <dirent.h>
+#include <stdlib.h>
 #include "message.h"
 
+void load_c_plugins(const char* dir) {
+   struct dirent **namelist;
+   int n = scandir(dir, &namelist, NULL, NULL);
+   if (n < 0) nv_err_msg("nvelox: Couldn't scan plugin directory.");
+   else {
+      while(n--) {
+         nv_out_msg(namelist[n]->d_name);      
+	 free(namelist[n]);
+      }
+      free(namelist);
+   }
+}
+
 void start_point(){
+   load_c_plugins(getenv("NVELOX_PLUGIN_DIR"));
    nv_out_msg("nvelox is loaded");
 }
