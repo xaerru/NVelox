717a8cff42efb9d0b9c86a5d14a136e7d2c40615
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-27 22:21:29 +0530

    feat(nix): update nvelox

--- nix/nvelox-neovim.nix
+++ nix/nvelox-neovim.nix
@@ -5,8 +5,8 @@ pkgs.neovim-unwrapped.overrideAttrs (oa: {
   src = fetchFromSourcehut {
     owner = "~xaerru";
     repo = "nvelox-neovim";
-    rev = "a2894e0bc91c81fea68d74e15c80bbc1a58d77d8";
-    sha256 = "sha256-TtF7ptoNTo7jJaFhnUnJQ9dIObigSgrgYxyUBjXCgSc";
+    rev = "c409532744e5753b5a885de4670cc7a89a8e61f0";
+    sha256 = "sha256-eUlh5TjuJW8XdyDawqZPCsCjTY63WhfycLvVMzhNm2U";
   };
   postInstall = ''
     mv $out/bin/nvim $out/bin/nvlx
