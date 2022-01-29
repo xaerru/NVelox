e168dff1fa660724648c3d59d03fda81dbf03a04
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-29 23:19:44 +0530

    feat(message): add msg_attr_keep function

--- src/nvelox/message.h
+++ src/nvelox/message.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
     char *data;
@@ -7,6 +8,8 @@ typedef struct {
 
 extern void nvim_out_write (String str);
 extern void nvim_err_write (String str);
+extern int msg(char *s);
+extern bool msg_attr_keep(const char *s, int attr, bool keep, bool multiline);
 
 void nv_out_msg (char *msg);
 void nv_err_msg (char *msg);
