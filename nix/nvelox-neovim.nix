d0c4c824686081aa10e0a73ec675679203193f44
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-03 15:04:21 +0530

    feat(nix): make src point to the git submodule

--- nix/nvelox-neovim.nix
+++ nix/nvelox-neovim.nix
@@ -2,12 +2,7 @@ with (import <nixpkgs> {});
 
 pkgs.neovim-unwrapped.overrideAttrs (oa: {
   name = "nvelox-neovim";
-  src = fetchFromSourcehut {
-    owner = "~xaerru";
-    repo = "nvelox-neovim";
-    rev = "a283565ca8d0ec4d848222c17b38239d0096c978";
-    sha256 = "sha256-R88oi6U2L74FjCycmf9CdUUYtwW7tGCOgS2cteVZJ88";
-  };
+  src = ../external/nvelox-neovim;
   cmakeFlags = oa.cmakeFlags ++ ["-DCMAKE_BUILD_TYPE=Debug"];
   postInstall = ''
     mv $out/bin/nvim $out/bin/nvlx
