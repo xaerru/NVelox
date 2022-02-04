de53e00b3815ffa229ed02758cad86e13f7af1a5
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-04 16:52:32 +0530

    feat(nix): update nix derivation to include dependencies for neovim
    New nix derivation
    
    Implements: https://todo.sr.ht/~xaerru/NVelox/2
    Signed-off-by: Gauravsingh Sisodia <xaerru@disroot.org>

--- nix/default.nix
+++ nix/default.nix
@@ -1,9 +1,14 @@
-with (import <nixpkgs> {});
+with (import <nixpkgs> { });
+with (pkgs.neovim-unwrapped);
 
 stdenv.mkDerivation {
   name = "nvelox";
-  src = builtins.fetchGit ../.;
-  buildInputs = with pkgs; [ cmake ];
+  src = builtins.fetchGit {
+    url = ../.;
+    submodules = true;
+  };
+  inherit lua buildInputs nativeBuildInputs dontFixCmake;
+  NVELOX_NEOVIM_CMAKE_FLAGS = builtins.concatStringsSep ";" cmakeFlags;
   makeTarget = "nvelox";
   enableParallelBuilding = true;
 }
