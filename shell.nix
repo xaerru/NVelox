e06f1cc8b35de10f278a8776b06b4b2126f23714
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-04 23:58:07 +0530

    feat(nix): callPackage ./nix/default.nix in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,16 +1,3 @@
-with (import <nixpkgs> {});
+{ pkgs ? import <nixpkgs> { } }:
 
-let
-nvelox-neovim = import ./nix/nvelox-neovim.nix;
-in
-mkShell {
-  nativeBuildInputs = [
-    cmake
-  ];
-  buildInputs = [
-    nvelox-neovim
-  ];
-  shellHook = ''
-    export NVELOX_PATH=build/
-  '';
-}
+pkgs.callPackage ./nix/default.nix { inherit pkgs; }
