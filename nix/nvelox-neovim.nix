39527d4261b5c62a9cd4cb2e1b72bba2f87757f5
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-05 11:30:15 +0530

    feat(nix): add rev to nvelox-neovim.nix

--- nix/nvelox-neovim.nix
+++ nix/nvelox-neovim.nix
@@ -2,6 +2,9 @@
 
 pkgs.neovim-unwrapped.overrideAttrs (oa: {
   name = "nvelox-neovim";
-  src = ../external/nvelox-neovim;
+  src = builtins.fetchGit {
+    url = ../external/nvelox-neovim;
+    rev = "a283565ca8d0ec4d848222c17b38239d0096c978";
+  };
   cmakeFlags = oa.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Debug" ];
 })
