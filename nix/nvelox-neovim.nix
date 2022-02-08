e6de9be198253f83c174eceef7f3c157a28074c4
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-08 19:15:09 +0530

    feat(nix): nvelox-neovim add debug flags

--- nix/nvelox-neovim.nix
+++ nix/nvelox-neovim.nix
@@ -3,5 +3,8 @@
 pkgs.neovim-unwrapped.overrideAttrs (oa: {
   name = "nvelox-neovim";
   src = ../external/nvelox-neovim;
+  dontStrip = true;
+  NIX_CFLAGS_COMPILE = " -ggdb3 -Og";
   cmakeFlags = oa.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Debug" ];
+  disallowedReferences = [];
 })
