7d88038dfb22abb7c992da9ccbf4977fa75f248f
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-16 12:13:00 +0530

    feat(nix): add gitignoreSource so that nix-build suceeds even when the build/ directory is present

--- nix/default.nix
+++ nix/default.nix
@@ -3,9 +3,16 @@
 let
   neovim = pkgs.neovim-unwrapped;
   nvelox-neovim = import ./nvelox-neovim.nix { inherit pkgs; };
+  gitignoreSrc = pkgs.fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "gitignore.nix";
+    rev = "5b9e0ff9d3b551234b4f3eb3983744fa354b17f1";
+    sha256 = "sha256-o/BdVjNwcB6jOmzZjOH703BesSkkS5O7ej3xhyO8hAY=";
+  };
+  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
 in pkgs.stdenv.mkDerivation {
   name = "nvelox";
-  src = ../.;
+  src = gitignoreSource ../.;
   buildInputs = neovim.buildInputs ++ [ nvelox-neovim ];
   dontFixCmake = true;
   nativeBuildInputs = neovim.nativeBuildInputs;
--- nix/nvelox-neovim.nix
+++ nix/nvelox-neovim.nix
@@ -1,8 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 
+let
+  gitignoreSrc = pkgs.fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "gitignore.nix";
+    rev = "5b9e0ff9d3b551234b4f3eb3983744fa354b17f1";
+    sha256 = "sha256-o/BdVjNwcB6jOmzZjOH703BesSkkS5O7ej3xhyO8hAY=";
+  };
+  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
+in
 pkgs.neovim-unwrapped.overrideAttrs (oa: {
   name = "nvelox-neovim";
-  src = ../external/nvelox-neovim;
+  src = gitignoreSource ../external/nvelox-neovim;
   dontStrip = true;
   NIX_CFLAGS_COMPILE = " -ggdb3 -Og";
   cmakeFlags = oa.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Debug" ];
