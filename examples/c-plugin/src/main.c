bf7dfc54910c58c0830090c49c565109fe2f3171
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-17 10:30:49 +0530

    feat(example): use multiqueue_put_event to demonstrate headers are working

--- examples/c-plugin/src/main.c
+++ examples/c-plugin/src/main.c
@@ -3,7 +3,13 @@
 #include <nvim/event/loop.h>
 #include <nvim/event/multiqueue.h>
 
+void print_msg(void **argv) {
+    char *str = argv[0];
+    nv_out_msg(str);
+}
+
 int nvelox_plugin_init() {
-    nv_out_msg ("This is an example C plugin.");
+    char *test_str = "multiqueue_put_event works";
+    multiqueue_put_event(main_loop.events, event_create(print_msg, 1, test_str));
     return 0;
 }
