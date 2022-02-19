1afe0d510a39260bd2942d6855818181dd12f745
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-20 00:15:06 +0530

    refactor: add header guards

--- src/nvelox/message.h
+++ src/nvelox/message.h
@@ -1,5 +1,10 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
 #include <stdbool.h>
 #include <stdlib.h>
 
 void nv_out_msg (char *msg);
 void nv_err_msg (char *msg);
+
+#endif  // MESSAGE_H
--- src/nvelox/runtime.h
+++ src/nvelox/runtime.h
@@ -1,4 +1,9 @@
+#ifndef RUNTIME_H
+#define RUNTIME_H
+
 void nv_load_plugins ();
 
 void **plugin_handles;
 int plugin_count;
+
+#endif  // RUNTIME_H
