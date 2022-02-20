09a18c1c77971381eb15d83ca2362e9b2b0a9d82
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-20 22:04:32 +0530

    refactor: add header guards to NVELOX_LOOP_H

--- src/nvelox/loop.h
+++ src/nvelox/loop.h
@@ -1,5 +1,10 @@
+#ifndef NVELOX_LOOP_H
+#define NVELOX_LOOP_H
+
 #include <nvim/main.h>
 
 uv_loop_t *nv_uv_loop ();
 
 void nv_schedule (argv_callback cb, int argc, ...);
+
+#endif  // NVELOX_LOOP_H
