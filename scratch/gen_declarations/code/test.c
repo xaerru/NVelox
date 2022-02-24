862eff7c2b04394c572b6bc75a3ff215b823bee4
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-24 20:31:46 +0530

    feat(scratch): add gen_declarations

---
+++ scratch/gen_declarations/code/test.c
@@ -0,0 +1,20 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+typedef struct {
+    int num;
+    int abc;
+} Name;
+
+typedef struct Something {
+    int num;
+    int abc;
+} Name2;
+
+struct Person {
+    int num;
+};
+
+#define ABC(a, b) ab
+#define A 1
